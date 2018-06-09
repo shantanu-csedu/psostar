7dc231816a24a5cfb7cdaac5b6a09037008f73c2
shantanu saha <shantanucse18@gmail.com>
2018-06-09 22:32:02 +0600

    implement other functions

--- src/main/java/com/simplesln/fitness/Ackley.java
+++ src/main/java/com/simplesln/fitness/Ackley.java
@@ -3,11 +3,26 @@ package com.simplesln.fitness;
 import com.simplesln.data.Particle;
 
 public class Ackley extends FitnessFunction{
+    private static double a = 20;
+    private static double b = 0.2;
+    private static double c = 2.0*Math.PI;
+
+
     Ackley(double l, double u) {
         super(l,u);
     }
 
-    public double fitness(Particle particle) {
-        return 0;
+    public double fitness(Particle particle)
+    {
+        double sum = 0;
+        double sumCos = 0;
+        for(int i=0;i<particle.dimension;i++){
+            sum += (particle.location.x[i] * particle.location.x[i]);
+            sumCos += Math.cos(c*particle.location.x[i]);
+        }
+        double result = -a * Math.exp(
+                -b * Math.sqrt((1.0/particle.dimension)*sum)
+        ) - Math.exp((1.0/particle.dimension) * sumCos) + a + Math.exp(1.0);
+        return result;
     }
 }
--- src/main/java/com/simplesln/fitness/Griewank.java
+++ src/main/java/com/simplesln/fitness/Griewank.java
@@ -8,6 +8,12 @@ public class Griewank extends FitnessFunction{
     }
 
     public double fitness(Particle particle) {
-        return 0;
+        double sum = 0;
+        double prod = 1;
+        for(int i=0;i<particle.dimension;i++){
+            sum += (particle.location.x[i]*particle.location.x[i]) / 4000.0;
+            prod *= (Math.cos(particle.location.x[i] / Math.sqrt(i+1)) + 1.0);
+        }
+        return (sum - prod);
     }
 }
